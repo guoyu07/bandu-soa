c828e892006a5b5392f7a9f83576519eb36a445c
Suhmayah Banda <su@aboynamedsu.net>
2014-01-13 14:06:18 +0000

    Added rendering functionality to Structs. Currently only supports JSON.

--- controllers/restful/UserController.php
+++ controllers/restful/UserController.php
@@ -2,22 +2,43 @@
 
 namespace Parplu\Controllers\RESTful;
 
+use parplu\resources\User;
+
+use parplu\resources\managers\UsersManager;
+
+use bandu\database\MySQLWrapper;
+
 class UserController extends Controller {
-    
+
     public function handleGet() {
-        return "get";
+        $db = new MySQLWrapper(array(
+                'server' => 'localhost',
+                'user' => 'root',
+                'password' => 'p4$$word!',
+                'db' => 'parplu'
+        ));
+
+        $rm = new UsersManager($db);
+
+        $user = new User(array(
+                'id' => '1',
+        ));
+
+        $rm->retrieve($user);
+        
+        return $user->render('JSON');
     }
-    
+
     public function handlePost() {
         return "post";
     }
-    
+
     public function handlePut() {
         return "put";
     }
-    
+
     public function handleDelete() {
         return "delete";
     }
-    
+
 }
